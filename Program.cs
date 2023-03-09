2450d0cbc7183b643bfde0373254626d071df5c5
Cheese <sweet_route@naver.com>
2023-03-09 15:12:13 +0900

    알고리즘 1회 문제풀이

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodayAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MissingNumbers test = new MissingNumbers();
+            test.MissingNumbers1();
+            //test.MissingNumbers2();
+            //test.MissingNumbers3();
+            //test.MissingNumbers4();
+        }
+    }
+}
